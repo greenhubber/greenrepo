39bbe94ce77135bfdd994bc1f3cd8a6ae39d8b30
root <root@localhost.localdomain>
2015-07-01 21:44:38 +0000

    changes were made

--- modules/ifconfig.py
+++ modules/ifconfig.py
@@ -1,7 +1,6 @@
 import os
-import subprocess
 
 def run(**args):
 	print "[*] in ifconfig module"
-	output = popen(["ifconfig"], stdout=PIPE).communicate()[0]
+	output = os.popen(["ifconfig"], stdout=PIPE).communicate()[0]
 	return output
