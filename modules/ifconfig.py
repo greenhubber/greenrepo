a50df5ad2eb0d74f40cbca32ba6c5b23959988d8
root <root@localhost.localdomain>
2015-07-01 21:30:24 +0000

    new module, Yay

--- modules/ifconfig.py
+++ modules/ifconfig.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 
 def run(**args):
-	output = Popen(["mycmd", "myarg"], stdout=PIPE).communicate()[0]
+	output = Popen(["ifconfig"], stdout=PIPE).communicate()[0]
 	return str(output)
